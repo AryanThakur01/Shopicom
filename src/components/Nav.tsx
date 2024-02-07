ae837e6bf60758be7de11a0d564763556ae3f78d
Aryan Thakur <aryan197297@gmail.com>
2024-02-07 23:46:36 +0530

    refactor: the middleware code naming

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,28 +1,32 @@
 "use client";
 import Link from "next/link";
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { LuSearch, LuShoppingBag } from "react-icons/lu";
 import { twMerge } from "tailwind-merge";
+import Cookies from "js-cookie";
 
 const Nav = () => {
-  // const cookies = document.cookie.split(";");
   return (
-    <nav className="container h-12 flex items-center bg-black/40 backdrop-blur-xl">
-      <Link href="/">Shopicom</Link>
-      <ul className="flex items-center gap-4 justify-end ml-auto">
-        <NavButton>
-          <LuSearch />
-        </NavButton>
-        <NavButton>
-          <LuShoppingBag />
-        </NavButton>
-        <Link
-          href="/login"
-          className="bg-muted text-foreground py-0.5 px-3 rounded text-md"
-        >
-          Login
-        </Link>
-      </ul>
+    <nav className="flex flex-col container h-12 bg-black/40 backdrop-blur-xl sticky top-0">
+      <div className="my-auto flex items-center">
+        <Link href="/">Shopicom</Link>
+        <ul className="flex items-center gap-4 justify-end ml-auto">
+          <button>
+            <NavButton>
+              <LuSearch />
+            </NavButton>
+          </button>
+          <NavButton>
+            <LuShoppingBag />
+          </NavButton>
+          <Link
+            href="/login"
+            className="bg-muted text-foreground py-0.5 px-3 rounded text-md"
+          >
+            Login
+          </Link>
+        </ul>
+      </div>
     </nav>
   );
 };
@@ -33,14 +37,14 @@ interface INavButton {
 }
 const NavButton: React.FC<INavButton> = ({ children, className }) => {
   return (
-    <button
+    <div
       className={twMerge(
         "flex justify-center items-center text-xl text-muted-foreground hover:text-foreground transition-all duration-500 p-1",
         className,
       )}
     >
       {children}
-    </button>
+    </div>
   );
 };
 
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,14 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const protectedRoutes = ["/login", "/register"];
+const hiddenRoutes = ["/login", "/register"];
 const privateRoutes = ["/dashboard/*"];
 
 export async function middleware(request: NextRequest) {
   const url = new URL(request.url, request.url);
   const cookie = cookies().get("Session_Token");
 
-  if (protectedRoutes.includes(url.pathname) && !!cookie)
+  if (hiddenRoutes.includes(url.pathname) && !!cookie)
     return NextResponse.redirect(new URL("/", request.url));
 
   for (const path of privateRoutes) {
