4a5cc56ab91cc9628aad95146ec984749a9753e8
Aryan Thakur <aryan197297@gmail.com>
2025-07-12 13:59:47 +0530

    add next public charable key for stripe

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,5 +3,4 @@ export const url =
     ? "https://shopicom.aryanthakur.dev"
     : "http://localhost:3000";
 
-export const stripePublicKey =
-  "pk_test_51Opr9ISDj5t8Jhb2a48qrDKxDliLUQ1cFHh0ouhfyxoXhGwGO8chczqnbtHlf0q55g7ELjyglb3J0kt6W3NagtdG00zkiFAkBW";
+export const stripePublicKey = String(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
