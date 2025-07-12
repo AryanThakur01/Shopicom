5efc5b9dcc6e6607c326108775734c04e3670c0b
Aryan Thakur <aryan197297@gmail.com>
2025-07-12 13:07:03 +0530

    remove: nextssr plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import Banner from "@/components/Home/Banner";
 import BestSellers from "@/components/Home/BestSellers";
 import Sponsored from "@/components/Home/Sponsored";
-import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
-import { extractRouterConfig } from "uploadthing/server";
-import { ourFileRouter } from "./api/uploadthing/core";
 
 export const dynamic = "force-dynamic";
 export default function Home() {
   return (
     <main className="bg-background">
-      <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
       <Banner />
       <Sponsored />
       <BestSellers />
